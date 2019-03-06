210eae5edb24cc2d22e696a9b66a1ea459eb684c
Castello Govender <cgovender@retrorabbit.co.za>
2019-03-06 12:26:23 +0200

    Working tutorial baseline project

---
+++ EchoBot1/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace EchoBot1
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int RandomText { get; set; }
+    }
+}
